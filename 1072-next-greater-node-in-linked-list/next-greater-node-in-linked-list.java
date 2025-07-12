c41c5d28d27b126ef19f5d9389d824f4643e34dc
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-07-12 08:02:58 +0530

    Time: 23 ms (64.89%) | Memory: 48.2 MB (10.79%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,23 +10,38 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        List<Integer> values = new ArrayList<>();
-        while (head != null) {
-            values.add(head.val);
-            head = head.next;
-        }
+        // List<Integer> values = new ArrayList<>();
+        // while (head != null) {
+        //     values.add(head.val);
+        //     head = head.next;
+        // }
 
-        int n = values.size();
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            int current = values.get(i);
-            for (int j = i + 1; j < n; j++) {
-                if (values.get(j) > current) {
-                    result[i] = values.get(j);
-                    break;
-                }
-            }
+        // int n = values.size();
+        // int[] result = new int[n];
+        // for (int i = 0; i < n; i++) {
+        //     int current = values.get(i);
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (values.get(j) > current) {
+        //             result[i] = values.get(j);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return result;
+        ArrayList <Integer> list = new ArrayList<>();
+        ListNode temp = head;
+        while(temp!=null){
+            list.add(temp.val);
+            temp = temp.next;
         }
-        return result;
+        int[] ans = new int[list.size()];
+        Stack <Integer> st = new Stack<>();
+        for(int i=0;i<list.size();i++){
+           while(!st.isEmpty() && list.get(st.peek())<list.get(i)){
+              ans[st.pop()]=list.get(i);
+           }
+           st.push(i);
+        }
+        return ans;
     }
 }
