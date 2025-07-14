2190258518b3493248a87b7ed0fe0e3ff521cd27
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-07-15 01:45:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (37.55%) - LeetSync

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int result=0;
+        ListNode current=head;
+
+        while(current != null){
+            result=result*2+current.val;
+            current=current.next;
+        }
+        return result;
+    }
+}
