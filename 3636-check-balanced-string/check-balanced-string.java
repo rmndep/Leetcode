38b9de16fe53ae92a7331d084385f84d34053a79
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-07-31 00:48:06 +0530

    Time: 1 ms (97.78%) | Memory: 42.3 MB (59.80%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isBalanced(String num) {
+        int eS=0;
+        int oS=0;
+
+        for(int i=0;i<num.length();i++){
+            int digit=num.charAt(i)-'0';
+            if(i%2==0){
+                eS+=digit;
+            }else{
+                oS+=digit;
+            }
+        }
+        return eS==oS;
+    }
+}
