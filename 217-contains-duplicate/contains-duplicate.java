332d7284f29a469c9428b0165de661163fea679b
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-09-15 01:35:22 +0530

    Time: 15 ms (47.65%) | Memory: 57.7 MB (92.75%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -10,13 +10,25 @@ class Solution {
         // }
         
         // return false;
-        Arrays.sort(nums);
-        int count=0;
-        int n=nums.length;
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
+     
+// eficient than brute force, this has tc-> o(n log(n))), brute force has o(n2)
+        // Arrays.sort(nums);
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==nums[i-1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+// using hashset -> as hashset doesnt contain duplicates! tc--> o(n)
+
+        HashSet<Integer> seenNum=new HashSet<>();
+
+        for(int num:nums){
+            if(seenNum.contains(num)){
                 return true;
             }
+            seenNum.add(num);
         }
         return false;
     }
