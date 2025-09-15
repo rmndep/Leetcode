a61044762ce7b828e555038cb2c2c6613bdb4b28
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-09-15 07:58:39 +0530

    Time: 14 ms (57.29%) | Memory: 49 MB (25.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,21 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int count=1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 1;
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        System.out.println(map);
+        // System.out.println(map);
 
-        
-         Integer[] keys = map.keySet().toArray(new Integer[0]);
+        //  Integer[] keys = map.keySet().toArray(new Integer[0]);
 
-        Arrays.sort(keys, (a, b) -> map.get(b) - map.get(a));
+        // Arrays.sort(keys, (a, b) -> map.get(b) - map.get(a));
 
-        int[] arr = new int[k];
-        for (int i = 0; i < k; i++) {
-            arr[i] = keys[i];
+        // int[] arr = new int[k];
+        // for (int i = 0; i < k; i++) {
+        //     arr[i] = keys[i];
+        // }
+        // return arr;
+
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        for (int n : map.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
         }
-        return arr;
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = heap.poll();
+        }
+        return ans;
     }
 }
