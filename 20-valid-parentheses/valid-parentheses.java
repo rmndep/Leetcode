b543e4560ab993c3eb94288d903cdc4ea2fd1162
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-09-26 18:16:53 +0530

    Time: 5 ms (8.64%) | Memory: 41.8 MB (62.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,24 +1,48 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack=new Stack<>();
+        // Stack<Character> stack=new Stack<>();
 
-        for(char ch:s.toCharArray()){
+        // for(char ch:s.toCharArray()){
             
-            if(ch=='(' || ch=='{' || ch=='['){
-                stack.push(ch);
+        //     if(ch=='(' || ch=='{' || ch=='['){
+        //         stack.push(ch);
+        //     }
+        //     else{
+        //         if(stack.isEmpty()) return false;
+
+        //         char top=stack.pop();
+        //         if((ch==')' && top !='(') ||
+        //         (ch=='}' && top !='{')||
+        //         (ch==']' && top !='[')){
+        //             return false;
+        //         }
+        //     }
+        // }
+
+        // return stack.isEmpty();
+        HashMap<Character, Character> bracs=new HashMap<>();
+        bracs.put('(',')');
+        bracs.put('[',']');
+        bracs.put('{','}');
+
+        Stack<Character> st=new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+
+            if(bracs.containsKey(c)){
+                st.push(c);
             }
             else{
-                if(stack.isEmpty()) return false;
-
-                char top=stack.pop();
-                if((ch==')' && top !='(') ||
-                (ch=='}' && top !='{')||
-                (ch==']' && top !='[')){
+                if(st.empty()){
+                    return false;
+                }
+                char top=st.pop();
+                if(c!=bracs.get(top)){
                     return false;
                 }
             }
         }
-
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
