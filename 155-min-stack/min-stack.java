86c6c388001071e2240c8a847c8633654c5a3894
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-09-27 11:36:49 +0530

    Time: 3 ms (100.00%) | Memory: 44.7 MB (89.06%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,41 +1,83 @@
+// class MinStack {
+//     Stack<Integer> stack;
+//     Stack<Integer> minstack;
+
+//     public MinStack() {
+//         stack=new Stack<>();
+//         minstack=new Stack<>();
+
+//     }
+    
+//     public void push(int val) {
+//         stack.push(val);
+//         if(minstack.isEmpty() || val<=minstack.peek()){
+//             minstack.push(val);
+//         }
+//     }
+    
+//     public void pop() {
+//         int popped=stack.pop();
+//         if(popped==minstack.peek()){
+//             minstack.pop();
+//         }
+//     }
+    
+//     public int top() {
+//         return stack.peek();
+//     }
+    
+//     public int getMin() {
+//         return minstack.peek();
+//     }
+// }
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack obj = new MinStack();
+//  * obj.push(val);
+//  * obj.pop();
+//  * int param_3 = obj.top();
+//  * int param_4 = obj.getMin();
+//  */
 class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> minstack;
+    private Node head;
 
     public MinStack() {
-        stack=new Stack<>();
-        minstack=new Stack<>();
+        
+        
 
     }
     
     public void push(int val) {
-        stack.push(val);
-        if(minstack.isEmpty() || val<=minstack.peek()){
-            minstack.push(val);
-        }
+      
+      if(head==null){
+        head =new Node(val,val,null);
+      }else{
+        head=new Node(val,Math.min(val,head.min),head);
+      }
     }
     
     public void pop() {
-        int popped=stack.pop();
-        if(popped==minstack.peek()){
-            minstack.pop();
-        }
+       head=head.next;
     }
     
     public int top() {
-        return stack.peek();
+        return head.val;
     }
     
     public int getMin() {
-        return minstack.peek();
+    return head.min;
+    }
+
+    public class Node{
+        int val;
+        int min;
+        Node next;
+
+        Node(int val, int min, Node next){
+            this.val=val;
+            this.min=min;
+            this.next=next;
+        }
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
