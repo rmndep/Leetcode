bbed8a1015831ac37bb616e4cc5219b2c21ad968
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-10-05 02:36:38 +0530

    Time: 11 ms (30.72%) | Memory: 42.6 MB (45.50%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String numberToWords(int n) {
+          if (n == 0) 
+            return "Zero";
+        String[] units = {
+            "",        "One",       "Two",      "Three",
+            "Four",    "Five",      "Six",      "Seven",
+            "Eight",   "Nine",      "Ten",      "Eleven",
+            "Twelve",  "Thirteen",  "Fourteen", "Fifteen",
+            "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+        };
+            
+        String[] tens = { 
+            "",     "",     "Twenty",  "Thirty", "Forty",
+            "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" 
+        };
+        
+        String[] multiplier = 
+                    {"", "Thousand", "Million", "Billion"};
+      
+        String res = "";
+        int group = 0;
+        
+        while (n > 0) {
+            if (n % 1000 != 0) {
+                
+                int value = n % 1000;
+                String temp = "";
+                
+                if (value >= 100) {
+                    temp = units[value / 100] + " Hundred ";
+                    value %= 100;
+                }
+                if (value >= 20) {
+                    temp += tens[value / 10] + " ";
+                    value %= 10;
+                }
+
+                if (value > 0) {
+                    temp += units[value] + " ";
+                }
+                temp += multiplier[group] + " ";
+
+                res = temp + res;
+            }
+            n /= 1000;
+            group++;
+        }
+        
+        return res.trim();
+    }
+}
