6ce05cec49cc2252dcbb362e84db51425465d60d
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-10-05 18:42:02 +0530

    Time: 1 ms (48.10%) | Memory: 42.6 MB (93.57%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,34 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result=new ArrayList<>();
-        result.add(new ArrayList<>());
+        // result.add(new ArrayList<>());
 
-        for(int num: nums){
-            int size=result.size();
-            for(int i=0;i<size;i++){
-                List<Integer> sub=new ArrayList<>(result.get(i));
-                sub.add(num);
-                result.add(sub);
-            }
-        }
+        // for(int num: nums){
+        //     int size=result.size();
+        //     for(int i=0;i<size;i++){
+        //         List<Integer> sub=new ArrayList<>(result.get(i));
+        //         sub.add(num);
+        //         result.add(sub);
+        //     }
+        // }
+        // return result;
+        List<Integer> arr=new ArrayList<>();
+        int index=0;
+        solve(nums, arr, index, result);
         return result;
+    }
+    private static void solve(int[] nums, List<Integer> arr, int index, List<List<Integer>> result){
+        if(index==nums.length){
+            result.add(new ArrayList<>(arr));
+            return;
+        }
+        //exclude
+        solve(nums,arr,index+1,result);
+        //include
+        int num=nums[index];
+        arr.add(num);
+        solve(nums,arr,index+1, result);
+        arr.remove(arr.size() - 1);
+
     }
 }
