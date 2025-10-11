eb4c3bcc2a1353c949aadd30a807dee0f22e587f
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-10-11 21:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (24.99%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -10,26 +10,46 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head==null) return null;
-        ListNode curr=head;
-        Map<Integer,Integer> count=new HashMap<>();
-        while(curr!=null){
-            count.put(curr.val,count.getOrDefault(curr.val,0)+1);
-            curr=curr.next;
-        }
+        if (head == null)
+            return null;
+        // ListNode curr=head;
+        // Map<Integer,Integer> count=new HashMap<>();
+        // while(curr!=null){
+        //     count.put(curr.val,count.getOrDefault(curr.val,0)+1);
+        //     curr=curr.next;
+        // }
 
-        curr=head;
-        ListNode dummy=new ListNode(0);
-        ListNode temp=dummy;
+        // curr=head;
+        // ListNode dummy=new ListNode(0);
+        // ListNode temp=dummy;
 
-        while(curr!=null){
-            if(count.get(curr.val)==1){
-                temp.next=curr;
-                temp=temp.next;
+        // while(curr!=null){
+        //     if(count.get(curr.val)==1){
+        //         temp.next=curr;
+        //         temp=temp.next;
+        //     }
+        //     curr=curr.next;
+        // }
+        // temp.next=null;
+        // return dummy.next;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while (curr != null) {
+            if (curr.next != null && curr.val == curr.next.val) {
+                while (curr.next != null && curr.val == curr.next.val) {
+                    curr = curr.next;
+                }
+                prev.next = curr.next;
+            }else{
+                prev=prev.next;
             }
             curr=curr.next;
         }
-        temp.next=null;
         return dummy.next;
     }
 }
