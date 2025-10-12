c927b7ac55783f6065f8895d02dcefff757668dc
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-10-12 23:41:24 +0530

    Time: 3 ms (55.10%) | Memory: 41.8 MB (83.90%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,48 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        // Stack<Character> stack=new Stack<>();
-
-        // for(char ch:s.toCharArray()){
-            
-        //     if(ch=='(' || ch=='{' || ch=='['){
-        //         stack.push(ch);
-        //     }
-        //     else{
-        //         if(stack.isEmpty()) return false;
-
-        //         char top=stack.pop();
-        //         if((ch==')' && top !='(') ||
-        //         (ch=='}' && top !='{')||
-        //         (ch==']' && top !='[')){
-        //             return false;
-        //         }
-        //     }
-        // }
-
-        // return stack.isEmpty();
-        HashMap<Character, Character> bracs=new HashMap<>();
-        bracs.put('(',')');
-        bracs.put('[',']');
-        bracs.put('{','}');
-
         Stack<Character> st=new Stack<>();
+        int n=s.length();
 
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
 
-            if(bracs.containsKey(c)){
-                st.push(c);
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
             }
             else{
-                if(st.empty()){
+                if(st.isEmpty()) return false;
+                char top=st.peek();
+                if((ch == ')' && top == '(') ||
+                    (ch == ']' && top == '[') ||
+                    (ch == '}' && top == '{')){
+                    st.pop();
+                }else{
                     return false;
                 }
-                char top=st.pop();
-                if(c!=bracs.get(top)){
-                    return false;
-                }
-            }
+            }  
         }
-        return st.isEmpty();
+        if(st.size()>0) return false;
+        return true;
     }
 }
