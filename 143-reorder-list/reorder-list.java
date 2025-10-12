42d70cb441d9a0fba04ea6918932cf17af77f4f7
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-10-12 07:01:06 +0530

    Time: 2 ms (84.66%) | Memory: 48.1 MB (72.16%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,23 +10,68 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        Stack<ListNode> st = new Stack<>();
-        ListNode curr = head;
-        int count = 0;
-        while (curr != null) {
-            st.push(curr);
-            count++;
-            curr = curr.next;
+        // Stack<ListNode> st = new Stack<>();
+        // ListNode curr = head;
+        // int count = 0;
+        // while (curr != null) {
+        //     st.push(curr);
+        //     count++;
+        //     curr = curr.next;
+        // }
+        // // System.out.println(st);
+        // curr=head;
+        // for(int i=0;i<count/2;i++){
+        //     ListNode endNode=st.pop();
+        //     ListNode nextNode=curr.next;
+        //     curr.next=endNode;
+        //     endNode.next=nextNode;
+        //     curr=nextNode;
+        // }
+        //    curr.next=null;
+
+        if(head==null || head.next == null) return;
+
+        //find middle
+        ListNode mid=middle(head);
+
+        //reverse second half
+        ListNode right=reverse(mid.next);
+        mid.next = null;
+        ListNode left = head;
+        //make connections
+        while(right!=null){
+            ListNode rNext=right.next;
+            ListNode lNext=left.next;
+
+            left.next = right;
+            right.next = lNext;
+
+            left=lNext;
+            right=rNext;
         }
-        // System.out.println(st);
-        curr=head;
-        for(int i=0;i<count/2;i++){
-            ListNode endNode=st.pop();
-            ListNode nextNode=curr.next;
-            curr.next=endNode;
-            endNode.next=nextNode;
-            curr=nextNode;
+
+    }
+    public static ListNode middle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-           curr.next=null;
+        return slow;
+    }
+
+    public static ListNode reverse(ListNode head){
+        ListNode curr=head;
+        ListNode prev=null;
+        ListNode nxt=null;
+
+        while(curr!=null){
+            nxt=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
     }
 }
