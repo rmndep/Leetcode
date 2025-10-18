e70c18a1ddbd2cc9d5185f35482540f144375dc8
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-10-19 05:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (70.34%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,25 +14,28 @@
  * }
  */
 class Solution {
-    public static int height(TreeNode x){
-        if(x==null) return 1;
-        int leftH=height(x.left);
-        int rightH=height(x.right);
-        return 1+Math.max(leftH,rightH);
-    }
-    public boolean compare(TreeNode root1,TreeNode root2){
-        if (root1 == null && root2 == null) return true;
-        if (root1 == null || root2 == null) return false;
-        if (root1.val != root2.val) return false;
-        return compare(root1.left, root2.left) && compare(root1.right, root2.right);
-    }
+    // public static int height(TreeNode x){
+    //     if(x==null) return 1;
+    //     int leftH=height(x.left);
+    //     int rightH=height(x.right);
+    //     return 1+Math.max(leftH,rightH);
+    // }
+    // public boolean compare(TreeNode root1,TreeNode root2){
+    //     if (root1 == null && root2 == null) return true;
+    //     if (root1 == null || root2 == null) return false;
+    //     if (root1.val != root2.val) return false;
+    //     return compare(root1.left, root2.left) && compare(root1.right, root2.right);
+    // }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        int h1=height(p);
-        int h2=height(q);
+        // int h1=height(p);
+        // int h2=height(q);
 
-        if(h1==h2 && compare(p,q)){
-            return true;
-        }
-        return false;
+        // if(h1==h2 && compare(p,q)){
+        //     return true;
+        // }
+        // return false;
+        if(p==null || q==null) return p==q;
+
+        return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
