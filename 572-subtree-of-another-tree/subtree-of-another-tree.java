bf1815d9230fd0ce5ad9535b8ffe6e3c5f1e9805
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2025-11-02 00:45:56 +0530

    Time: 5 ms (8.72%) | Memory: 46.4 MB (6.39%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -14,28 +14,39 @@
  * }
  */
 class Solution {
-    public boolean isIdentical(TreeNode root, TreeNode subRoot){
-        if(root==null && subRoot==null){
-            return true;
-        }else if(root==null || subRoot==null || root.val != subRoot.val){
-            return false;
-        }
-        if(!isIdentical(root.left,subRoot.left)){
-            return false;
-        }
-        if(!isIdentical(root.right,subRoot.right)){
-            return false;
-        }
-        return true;
-    }
+    // public boolean isIdentical(TreeNode root, TreeNode subRoot){
+    //     if(root==null && subRoot==null){
+    //         return true;
+    //     }else if(root==null || subRoot==null || root.val != subRoot.val){
+    //         return false;
+    //     }
+    //     if(!isIdentical(root.left,subRoot.left)){
+    //         return false;
+    //     }
+    //     if(!isIdentical(root.right,subRoot.right)){
+    //         return false;
+    //     }
+    //     return true;
+    // }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null) return false;
+        // if(root==null) return false;
         
-        if(root.val==subRoot.val){
-            if(isIdentical(root,subRoot)){
-                return true;
-            }
-        }
+        // if(root.val==subRoot.val){
+        //     if(isIdentical(root,subRoot)){
+        //         return true;
+        //     }
+        // }
+        // return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
+        if(root==null) return false;
+        if(isSameTree(root,subRoot)) return true;
         return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
+    }
+    public boolean isSameTree(TreeNode root, TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        if(root==null||subRoot==null) return false;
+
+        if(root.val!=subRoot.val) return false;
+
+        return isSameTree(root.left,subRoot.left)&&isSameTree(root.right,subRoot.right);
     }
 }
