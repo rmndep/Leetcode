c62f32cbeb0cefeff004e4eab40412f332497ffd
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2026-01-15 02:19:25 +0530

    Time: 7 ms (3.11%) | Memory: 43.9 MB (5.22%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,13 +1,22 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-       return isSubsequenceHelper(s,t,0,0);
-    }
-      private boolean isSubsequenceHelper(String s, String t, int i, int j) {
-        if (i == s.length()) return true;
-        if (j == t.length()) return false;
-        if (s.charAt(i) == t.charAt(j)) {
-            return isSubsequenceHelper(s, t, i + 1, j + 1);
+    public boolean isSubsequence(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+
+        int dp[][]=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        return isSubsequenceHelper(s, t, i, j + 1);
-}
+        if(n==dp[n][m]){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
