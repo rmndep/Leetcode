356e5886e49ac2ba31efaeb06cbf2ffd2299ba5e
Ramandeep Singh <150382336+rmndep@users.noreply.github.com>
2026-04-26 17:47:46 +0530

    Time: 30 ms (27.83%) | Memory: 48.6 MB (84.84%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[] arr=new int[m];
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    arr[j]+=1;
+                }else{
+                    arr[j]=0;
+                }
+            }
+            maxArea=Math.max(maxArea,largest(arr));
+        }
+        return maxArea;
+    }
+    static int largest(int[] arr){
+        int n=arr.length;
+        int[] nsr=new int[n];
+        int[] nsl=new int[n];
+        Stack<Integer> st=new Stack<>();
+
+        //nsr
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nsr[i]=n;
+            }
+            else{
+                nsr[i]=st.peek();
+            }
+            st.push(i);
+        }
+        st.clear();
+        //nsl
+          for(int i=0;i<n;i++){
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                nsl[i]=-1;
+            }
+            else{
+                nsl[i]=st.peek();
+            }
+            st.push(i);
+        }
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            int w=nsr[i]-nsl[i]-1;
+            int a=w*arr[i];
+            maxArea=Math.max(maxArea,a);
+        }
+        return maxArea;
+    }
+}
